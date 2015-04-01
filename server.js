ade11e262c9fee29f7fbe78b4ca349812a88cb16
Rose DeMaio <io302rd@gold.ac.uk>
2015-04-01 13:28:43 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+// Look for openshift port and ip first, if not, host locally
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ip);
