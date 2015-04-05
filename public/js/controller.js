d890315fa029c764727ea0a8ef4783417d6f8393
Rose DeMaio <io302rd@gold.ac.uk>
2015-04-05 13:21:50 -0400

    add routing and prelim local auth

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,18 +3,62 @@ var app = angular.module("LunchboxApp", ["ngRoute"]);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
     when('/home', {
-        templateUrl: '../pages/home.html'
+        templateUrl: '../pages/home/home.html'
     }).
     when('/browse', {
-        templateUrl: '../pages/browse.html'
+        templateUrl: '../pages/browse/browse.html'
     }).
     when('/search', {
-        templateUrl: '../pages/search.html'
+        templateUrl: '../pages/search/search.html'
     }).
     when('/my-recipes', {
-        templateUrl: '../pages/my-recipes.html'
+        templateUrl: '../pages/my-recipes/my-recipes.html'
+    }).
+    when('/login', {
+        templateUrl: '../pages/login/login.html',
+        controller: 'LoginCtrl'
     }).
     otherwise({
         redirectTo: '/home'
     });
+
+    // $httpProvider
+    // .interceptors
+    // .push(function($q, $location)
+    // {
+    //     return {
+    //         response: function(response)
+    //         { 
+    //             return response;
+    //         },
+    //         responseError: function(response)
+    //         {
+    //             if (response.status === 401)
+    //                 $location.url('/login');
+    //             return $q.reject(response);
+    //         }
+    //     };
+    // }); 
 }]);
+
+var checkLoggedin = function($q, $timeout, $http, $location, $rootScope)
+{
+    var deferred = $q.defer();
+
+    $http.get('/loggedin').success(function(user)
+    {
+        $rootScope.errorMessage = null;
+        // User is Authenticated
+        if (user !== '0')
+            deferred.resolve();
+        // User is Not Authenticated
+        else
+        {
+            $rootScope.errorMessage = 'You need to log in.';
+            deferred.reject();
+            $location.url('/login');
+        }
+    });
+    
+    return deferred.promise;
+};
