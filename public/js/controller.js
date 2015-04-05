bf8ff59cd0fade262e3bc08f1e7b50426cf25a56
tswartz <tswartz92@gmail.com>
2015-04-05 12:03:32 -0400

    Added routing and pages dir

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,1 +1,20 @@
-"STAGE FRIGHT. SHOULD WE LET'S GET DAT ROUTING FROM CLASS INTO DIS AMIRIGHT"
+var app = angular.module("LunchboxApp", ["ngRoute"]);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    when('/home', {
+        templateUrl: '../pages/home.html'
+    }).
+    when('/browse', {
+        templateUrl: '../pages/browse.html'
+    }).
+    when('/search', {
+        templateUrl: '../pages/search.html'
+    }).
+    when('/my-recipes', {
+        templateUrl: '../pages/my-recipes.html'
+    }).
+    otherwise({
+        redirectTo: '/home'
+    });
+}]);
