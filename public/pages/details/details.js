22cb397a0a313a4603de6348365bde60a7a7ae74
Rose DeMaio <io302rd@gold.ac.uk>
2015-04-10 14:24:58 -0400

    consistency with http requests and white background for navbar

--- public/pages/details/details.js
+++ public/pages/details/details.js
@@ -1,9 +1,14 @@
 app.controller("DetailsCtrl", function ($scope, $http, $routeParams) {
     var recipeId = $routeParams.recipeId;
-    $http.jsonp("http://api.yummly.com/v1/api/recipe/" + recipeId + "?_app_id=6e96cfda&_app_key=555f5bd10dffba7229ea6a9ec32c0705&callback=JSON_CALLBACK")
+    var params = {
+        _app_id: "6e96cfda",
+        _app_key: "555f5bd10dffba7229ea6a9ec32c0705",
+        callback: "JSON_CALLBACK"
+    }
+    $http.jsonp("http://api.yummly.com/v1/api/recipe/" + recipeId + "?" + $.param(params))
     .success(function (response) {
-    	console.log(response);
-        // $scope.$recipe = response;
+        console.log(response);
+        $scope.recipe = response;
     })
     .error(function (data) {
         $scope.errorMessage = data;
