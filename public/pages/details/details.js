4d0783b4c7ac1d03ea19388990508cf058dc27b7
Rose DeMaio <io302rd@gold.ac.uk>
2015-04-10 14:04:24 -0400

    adding details page

---
+++ public/pages/details/details.js
@@ -0,0 +1,13 @@
+app.controller("DetailsCtrl", function ($scope, $http, $routeParams) {
+    var recipeId = $routeParams.recipeId;
+    // var recipeId = "French-Onion-Soup-1019866"
+    $http.jsonp("http://api.yummly.com/v1/api/recipe/" + recipeId + "?_app_id=6e96cfda&_app_key=555f5bd10dffba7229ea6a9ec32c0705&callback=JSON_CALLBACK")
+    .success(function (response) {
+    	console.log(response);
+        // $scope.$recipe = response;
+    })
+    .error(function (data) {
+        $scope.errorMessage = data;
+    });
+
+})
