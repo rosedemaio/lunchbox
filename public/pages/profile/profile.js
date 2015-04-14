b896b26b9b8fee89de6a9d38323c003fd69bb06b
tswartz <tswartz92@gmail.com>
2015-04-13 22:17:21 -0400

    Un-hardcoded profile's reviews

--- public/pages/profile/profile.js
+++ public/pages/profile/profile.js
@@ -17,20 +17,6 @@ app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
 		if (!user) {
 			$scope.errorMessage = "User " + username + " not found";
 		} else {
-            user.reviews = [
-                {
-                    rating: 5,
-                    text: "This is recipe is so great OMGGGG I make it all the time it's soooooo delicious I just can't stop thinking about it",
-                    recipeId: "French-Onion-Soup-1019866",
-                    recipeName: "French Onion Soup"
-                },
-                {
-                    rating: 3,
-                    text: "This is recipe is kinda meh. Like whatever. I don't even care. Now I'm tired. I'm gonna take a nap kbyeeeeeeeeeee.",
-                    recipeId: "Irish-American-Onion-Soup-1061263",
-                    recipeName: "Irish American Onion Soup"
-                }
-            ];
 			$scope.profileUser = user;
 		}
 		setFollowButtons();
