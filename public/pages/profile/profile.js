1bc16b9986e27d381a8261b580b6603667b6ea0e
tswartz <tswartz92@gmail.com>
2015-04-16 15:25:26 -0400

    Documented profile

--- public/pages/profile/profile.js
+++ public/pages/profile/profile.js
@@ -1,8 +1,10 @@
 app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
 {
+    // get username from URL and see that user is currently logged in
 	var username = $routeParams.username;
 	$scope.isCurrentUser = $scope.$parent.user != '0' && username == $scope.$parent.user.username;
 
+    // The chevron next to "Following" on the profile should twirl down when clicked
     $('#followingCollapse').on('show.bs.collapse', function () {
         $('#collapseLink span').removeClass('fa-chevron-up');
         $('#collapseLink span').addClass('fa-chevron-down');
@@ -12,6 +14,9 @@ app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
         $('#collapseLink span').addClass('fa-chevron-up');
     });
 
+    // grab user from db
+    // $scope.profileUser -> user of currently-viewed profile
+    // $scope.$parent.user -> currently logged-in user
 	$http.get('/user/' + username)
 	.success(function (user) {
 		if (!user) {
@@ -19,12 +24,14 @@ app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
 		} else {
 			$scope.profileUser = user;
 		}
+        // Refresh follow button state
 		setFollowButtons();
     })
     .error(function (data) {
     	$scope.errorMessage = data;
     });
 
+    // Retrieves profileUser's reviews
     $http.get('/reviewsByUser/' + username)
     .success(function (reviews) {
         $scope.reviews = reviews;
@@ -33,6 +40,8 @@ app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
         $scope.errorMessage = data;
     })
 
+    // Called when logged-in user wants to follow 
+    // user in currently-viewed profile (profielUser)
     $scope.follow = function() {
     	$http.put('/follow', $scope.profileUser)
         .success(function (response) {
@@ -44,6 +53,8 @@ app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
         });
     };
 
+    // Called when logged-in user wants to unfollow 
+    // user in currently-viewed profile (profielUser)
     $scope.unfollow = function() {
     	$http.put('/unfollow', $scope.profileUser)
         .success(function (response) {
@@ -55,6 +66,9 @@ app.controller('ProfileCtrl', function ($scope, $http, $routeParams, $location)
         });
     };
 
+    // If logged-in user is not following user in currently-viewed profile, show FOLLOW button
+    // If logged-in user is following user in currently-viewed profile, show UNFOLLOW button
+    // Note: follow/unfollow button is only shown when a user is logged in
     var setFollowButtons = function () {
     	var isFollowingProfileUser = $scope.$parent.user != '0' && $scope.$parent.user.following.indexOf(username) != -1;
     	var loggedInAndNotViewingOwnProfile = !$scope.isCurrentUser && $scope.$parent.user!='0' && $scope.profileUser;
